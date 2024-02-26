3a177d24e8999426a294b223a71c66b6509f6715
Navi <navisenthilnadhan@gmail.com>
2024-02-26 08:12:51 +0530

    Changed frontend url

--- client/src/axiosConfig.js
+++ client/src/axiosConfig.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000', // Example base URL
+  // baseURL: 'http://localhost:5000', // Example base URL
+  baseURL:'https://employeesql.onrender.com',
   timeout: 5000, // Request timeout in milliseconds
 });
 
